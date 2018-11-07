9e5b321d1b14c2acf82fabc26e90868ca96f42d1
KornevL <kornevlev@icloud.com>
2018-11-07 23:54:25 +0300

    add first exam programm

---
+++ week_1/week_first_programm_first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << x + y;
+    return 0;
+}
+
+// /Users/lev/eclipse-workspace/cpp_example_vscode/week_1
